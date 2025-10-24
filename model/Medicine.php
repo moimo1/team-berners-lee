16b280dbacf0d0146dbc98e5885695bd4687fd5c
DANNIEVIC <2240879@slu.edu.ph>
2025-10-24 13:02:42 +0800

    Added Medicine model class

---
+++ model/Medicine.php
@@ -0,0 +1,66 @@
+<?php
+class Medicine {
+    private $id;
+    private $name;
+    private $brand;
+    private $manufactureDate;
+    private $expiryDate;
+    private $amount;
+    private $description;
+
+        public function getId() {
+        return $this->id;
+    }
+
+    public function setId($id) {
+        $this->id = $id;
+    }
+
+    public function getName() {
+        return $this->name;
+    }
+
+    public function setName($name) {
+        $this->name = $name;
+    }
+
+    public function getBrand() {
+        return $this->brand;
+    }
+
+    public function setBrand($brand) {
+        $this->brand = $brand;
+    }
+
+    public function getManufactureDate() {
+        return $this->manufactureDate;
+    }
+
+    public function setManufactureDate($manufactureDate) {
+        $this->manufactureDate = $manufactureDate;
+    }
+
+    public function getExpiryDate() {
+        return $this->expiryDate;
+    }
+
+    public function setExpiryDate($expiryDate) {
+        $this->expiryDate = $expiryDate;
+    }
+
+    public function getAmount() {
+        return $this->amount;
+    }
+
+    public function setAmount($amount) {
+        $this->amount = $amount;
+    }
+
+    public function getDescription() {
+        return $this->description;
+    }
+
+    public function setDescription($description) {
+        $this->description = $description;
+    }
+}
