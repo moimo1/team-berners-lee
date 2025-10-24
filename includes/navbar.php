d6a8fb7c85a7281e4b8e19589d8fa7cd575120d9
Renz Dadpaas <rdadpaas123@gmail.com>
2025-10-24 09:51:45 +0800

    created plain navbar

---
+++ includes/navbar.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Team Project</title>
+</head>
+<body>
+
+<nav class="navbar">
+    <div class="nav-logo">
+        <a href="../index.php">MyWebsite</a>
+    </div>
+    <ul class="nav-links">
+        <li><a href="../view/home.php">Home</a></li>
+        <li><a href="../view/about.php">About</a></li>
+        <li><a href="../view/contact.php">Contact</a></li>
+    </ul>
+</nav>
