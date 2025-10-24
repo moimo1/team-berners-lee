e90931c8414dd4ead7135d2d880116e5e0d19de3
DANNIEVIC <2240879@slu.edu.ph>
2025-10-24 13:07:07 +0800

    Added Prescription model class

---
+++ model/Prescription.php
@@ -0,0 +1,48 @@
+<?php
+class Prescription {
+    private $prescID;
+    private $doctorID;
+    private $clientID;
+    private $dateGiven;
+    private $dateExpiry;
+
+    public function getPrescID() {
+        return $this->prescID;
+    }
+
+    public function setPrescID($prescID) {
+        $this->prescID = $prescID;
+    }
+
+    public function getDoctorID() {
+        return $this->doctorID;
+    }
+
+    public function setDoctorID($doctorID) {
+        $this->doctorID = $doctorID;
+    }
+
+    public function getClientID() {
+        return $this->clientID;
+    }
+
+    public function setClientID($clientID) {
+        $this->clientID = $clientID;
+    }
+
+    public function getDateGiven() {
+        return $this->dateGiven;
+    }
+
+    public function setDateGiven($dateGiven) {
+        $this->dateGiven = $dateGiven;
+    }
+
+    public function getDateExpiry() {
+        return $this->dateExpiry;
+    }
+
+    public function setDateExpiry($dateExpiry) {
+        $this->dateExpiry = $dateExpiry;
+    }
+}
