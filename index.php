be3cdd113b71163c24417104e8cea156d0f59b23
Renz Dadpaas <rdadpaas123@gmail.com>
2025-10-29 00:16:40 +0800

    merge

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>My PHP Project</title>
+  <link rel="stylesheet" href="assets/css/auth/login.css">
 </head>
 <body>
 
