8eaae9752ecaa01a5ba17f8146e4655223824d89
Renz Dadpaas <rdadpaas123@gmail.com>
2025-10-29 10:46:10 +0800

    fixed db access issue; removed debugging messages

--- controller/get-prescription.php
+++ controller/get-prescription.php
@@ -4,15 +4,9 @@ session_start();
 include '../config/db_con.php';
 header('Content-Type: application/json');
 
-if (!isset($_SESSION['id']) || !isset($_SESSION['role']) || $_SESSION['role'] !== 'client') {
-    http_response_code(401);
-    echo json_encode([]);
-    exit;
-}
 
 $clientID = $_SESSION['id'];
 
-// Get the most recent prescription for this client
 $sql = "SELECT * FROM prescription WHERE clientID = ? ORDER BY dateGiven DESC LIMIT 1";
 $stmt = $conn->prepare($sql);
 if ($stmt === false) {
@@ -30,26 +24,13 @@ if (!$ok) {
 $result = $stmt->get_result();
 $prescription = $result->fetch_assoc();
 
-if (!$prescription) {
-    echo json_encode([]);
-    exit;
-}
-
 // Get details for the found prescription
 $sql_prescDetails = "SELECT * FROM prescriptiondetails WHERE prescID = ?";
 $stmt_details = $conn->prepare($sql_prescDetails);
-if ($stmt_details === false) {
-    http_response_code(500);
-    echo json_encode(["error" => "prepare_details_failed", "message" => $conn->error]);
-    exit;
-}
+
 $stmt_details->bind_param("s", $prescription["prescID"]);
 $ok2 = $stmt_details->execute();
-if (!$ok2) {
-    http_response_code(500);
-    echo json_encode(["error" => "execute_details_failed", "message" => $stmt_details->error]);
-    exit;
-}
+
 $result_details = $stmt_details->get_result();
 
 $data = [];
