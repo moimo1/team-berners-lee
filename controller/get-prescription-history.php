249530859c097fd6c62cc02081990b660ed3c567
Renz Dadpaas <rdadpaas123@gmail.com>
2025-10-30 14:55:40 +0800

    finished controller responsible for retrieving prescription history of client

--- controller/get-prescription-history.php
+++ controller/get-prescription-history.php
@@ -8,3 +8,13 @@ $clientID=$_SESSION['id'];
 $sql = "SELECT doctor.firstName, doctor.lastName, prescription.dateGiven, prescription.dateExpiry FROM doctor JOIN prescription ON doctor.doctorID = prescription.doctorID WHERE prescription.clientID=?";
 $stmt = $conn->prepare($sql);
 $stmt->bind_param("s", $clientID);
+$stmt->execute();
+$result = $stmt->get_result();
+
+$data =[];
+
+while ($row = $result->fetch_assoc()) {
+    $data[] = $row;
+}
+echo json_encode($data);
+?>
