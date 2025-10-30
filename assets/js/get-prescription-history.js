f756d0377e6d4f70b5e1c5cf3dbc49d2082a14da
Renz Dadpaas <rdadpaas123@gmail.com>
2025-10-30 19:01:40 +0800

    resolved php issues after the changes

--- assets/js/get-prescription-history.js
+++ assets/js/get-prescription-history.js
@@ -1,19 +1,18 @@
-
-
 document.addEventListener('DOMContentLoaded', () => {
     listContainer = document.querySelector('#prescription-history-tbody');
 
-    fetch('../controller/get-prescription-history.php', { credentials: 'same-origin' })
+    fetch('../../controller/get-prescription-history.php', { credentials: 'same-origin' })
     .then(res => {
         return res.json();
     })
     .then(data => {
+        console.log('Fetched data:', data);
         data.forEach(item => {
             const row = document.createElement('tr');
             row.innerHTML = `
-                <td>${item.firstName ?? ''} ${item.lastName ?? ''}</td>
                 <td>${item.dateGiven ?? ''}</td>
                 <td>${item.dateExpiry ?? ''}</td>
+                <td>${item.doctorName}</td>
             `;
             listContainer.appendChild(row);
         });
