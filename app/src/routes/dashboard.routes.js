a8912e6e48e4ee0d3e7a25648c59809d2687ec88
Renz Dadpaas <rdadpaas123@gmail.com>
2025-11-27 16:17:39 +0800

    created dashboard service

--- app/src/routes/dashboard.routes.js
+++ app/src/routes/dashboard.routes.js
@@ -1,8 +1,8 @@
-import express from 'express';
-import { getDashboard } from '../controllers/dashboard.controller.js';
+import express from "express";
+import { getDashboard } from "../controllers/dashboard.controller.js";
 
 const router = express.Router();
 
-router.get('/dashboard', getDashboard);
+router.get("/dashboard", getDashboard);
 
 export default router;
