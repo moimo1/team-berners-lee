2ad6e146d8b8439dd3fe831a67b3ed18d229b858
Renz Dadpaas <rdadpaas123@gmail.com>
2025-11-27 16:18:20 +0800

    fix server.js

--- app/server.js
+++ app/server.js
@@ -1,14 +1,11 @@
-import express from 'express';
-import cors from 'cors';
-import dashboardRoutes from './src/routes/dashboard.routes.js';
+import express from "express";
+import cors from "cors";
+import dashboardRoutes from "./src/routes/dashboard.routes.js";
 
 const app = express();
-
 app.use(cors({ origin: true, credentials: true }));
 app.use(express.json());
 
-app.use('/api/pharma-admin', dashboardRoutes);
+app.use("/api/pharma-admin", dashboardRoutes);
 
-app.listen(3000, () =>
-  console.log('Server running on http://localhost:3000')
-);
+app.listen(3000, () => console.log("Server listening on port 3000"));
