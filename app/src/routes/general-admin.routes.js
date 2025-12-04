75fbf27ec5e636a1593a5ec0258a2bdcb3361765
Renz Dadpaas <rdadpaas123@gmail.com>
2025-12-04 23:22:14 +0800

    update gen admin route

--- app/src/routes/general-admin.routes.js
+++ app/src/routes/general-admin.routes.js
@@ -3,7 +3,9 @@ import * as generalAdminController from '../controllers/general-admin.controller
 
 const router = express.Router();
 
+router.get('/dashboard', generalAdminController.getDashboard);
 router.get('/admins', generalAdminController.getPharmacistAdmins);
 router.get('/admins/:adminId/performance', generalAdminController.getAdminPerformance);
+router.get('/admins/:adminId/team', generalAdminController.getAdminTeam);
 
 export default router;
