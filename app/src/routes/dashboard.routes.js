51d8c467eb89c21fe14882c96cd5d632ce128529
Renz Dadpaas <rdadpaas123@gmail.com>
2025-12-04 16:00:51 +0800

    updated dashboard routes

--- app/src/routes/dashboard.routes.js
+++ app/src/routes/dashboard.routes.js
@@ -1,8 +1,10 @@
 import express from "express";
 import { getDashboard } from "../controllers/dashboard.controller.js";
+import prescriptionRoutes from "./prescription.routes";
 
 const router = express.Router();
 
 router.get("/dashboard", getDashboard);
+router.use('/prescriptions', prescriptionRoutes);
 
 export default router;
