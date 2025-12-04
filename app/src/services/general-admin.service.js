797c7142b5085048a8010350e2f67208433e4642
Renz Dadpaas <rdadpaas123@gmail.com>
2025-12-04 23:19:39 +0800

    created general admin service

---
+++ app/src/services/general-admin.service.js
@@ -0,0 +1,9 @@
+import * as generalAdminModel from '../models/general-admin.model.js';
+
+export async function getPharmacistAdmins() {
+  return await generalAdminModel.getPharmacistAdmins();
+}
+
+export async function getAdminPerformance(adminId) {
+  return await generalAdminModel.getAdminPerformance(adminId);
+}
