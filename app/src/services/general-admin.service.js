7471597d042f239b9a95aa8c0cd2e235eee478fa
Renz Dadpaas <rdadpaas123@gmail.com>
2025-12-04 23:21:37 +0800

    .

--- app/src/services/general-admin.service.js
+++ app/src/services/general-admin.service.js
@@ -1,9 +1,17 @@
 import * as generalAdminModel from '../models/general-admin.model.js';
 
+export async function getDashboard() {
+  return await generalAdminModel.getDashboard();
+}
+
 export async function getPharmacistAdmins() {
   return await generalAdminModel.getPharmacistAdmins();
 }
 
 export async function getAdminPerformance(adminId) {
   return await generalAdminModel.getAdminPerformance(adminId);
+}
+
+export async function getAdminTeam(adminId) {
+  return await generalAdminModel.getAdminTeam(adminId);
 }
