dfbfc7bfa47188a9a25c0e16c19b52b063025e9b
Renz Dadpaas <rdadpaas123@gmail.com>
2025-12-11 18:12:57 +0800

    registered route for admin

--- app/server.js
+++ app/server.js
@@ -4,6 +4,7 @@ import cors from 'cors';
 import dashboardRoutes from './src/routes/dashboard.routes.js';
 import prescriptionRoutes from './src/routes/prescriptions.routes.js';
 import historyRoutes from './src/routes/pharmacist-history.routes.js';
+import adminUserRoutes from './src/routes/admin-users.routes.js';
 
 const app = express();
 const PORT = 3000;
@@ -14,6 +15,7 @@ app.use(express.json());
 app.use('/api/pharma-admin/dashboard', dashboardRoutes);
 app.use('/api/pharma-admin/prescriptions', prescriptionRoutes);
 app.use('/api/pharma-admin/pharmacists', historyRoutes);
+app.use('/api/admin', adminUserRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
